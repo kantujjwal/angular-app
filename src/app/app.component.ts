a0da1654427c8c03572221bb568253218eabcc85
kantujjwal <ujjwal2154@gmail.com>
2024-05-12 11:31:46 +0530

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from "./servers/servers.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    // styleUrl: './app.component.scss',
+    styles: `
+    h3 {
+        color: dodgerblue;
+    }`,
+    standalone: true,
+    imports: [ RouterOutlet, ServerComponent, ServersComponent]
 })
 export class AppComponent {
-  title = 'Angular App';
 }
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ServerComponent } from "../server/server.component";
+
+@Component({
+    selector: 'app-servers',
+    standalone: true,
+    // templateUrl: './servers.component.html',
+    template: `
+    <app-server></app-server>
+    
+    <app-server></app-server>
+    `,
+    styleUrl: './servers.component.scss',
+    imports: [ServerComponent]
+})
+export class ServersComponent {
+
+}
